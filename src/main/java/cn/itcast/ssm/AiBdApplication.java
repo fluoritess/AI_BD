53b39fba219771f4507237df4dba953c52c3b71d
cdu_liu_yang <1138556554@qq.com>
2018-07-30 10:51:44 +0800

    Merge branch 'master' of https://gitee.com/Dust511/AI_BD
    # Conflicts:
    #	src/main/java/cn/itcast/ssm/service/impl/UserServiceImpl.java

--- src/main/java/cn/itcast/ssm/AiBdApplication.java
+++ src/main/java/cn/itcast/ssm/AiBdApplication.java
@@ -3,7 +3,12 @@ package cn.itcast.ssm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 @ServletComponentScan//拦截器扫描
@@ -11,6 +16,21 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 public class AiBdApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AiBdApplication.class, args);
+        SpringApplication app = new SpringApplication(AiBdApplication.class);
+        app.run(args);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+
+                container.addErrorPages(error404Page);
+            }
+        };
     }
 }
