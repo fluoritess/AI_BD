9581ba5612e8fd4b9f4a547c7f9443da63fcfab7
wangshanwen <302618914@qq.com>
2018-10-23 21:46:17 +0800

    统计收集数据

---
+++ src/main/java/cn/itcast/ssm/service/impl/StatisticalAnalysisServiceImpl.java
@@ -0,0 +1,79 @@
+package cn.itcast.ssm.service.impl;
+
+import cn.itcast.ssm.mapper.CollectInfoValueMapper;
+import cn.itcast.ssm.po.CollectInfoValue;
+import cn.itcast.ssm.service.StatisticalAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+@Service
+public class StatisticalAnalysisServiceImpl implements StatisticalAnalysisService {
+    @Autowired
+    CollectInfoValueMapper collectInfoValueMapper;
+    @Override
+    public  List<CollectInfoValue> StatisticalQuarter(java.util.Date date){
+        List<CollectInfoValue> list=collectInfoValueMapper.selectByExample(null);
+        List<CollectInfoValue> tarlist=new ArrayList<CollectInfoValue>();
+        Iterator<CollectInfoValue> it=list.iterator();
+        while(it.hasNext()){
+            CollectInfoValue collectInfoValue=it.next();
+            java.util.Date date1=collectInfoValue.getCollectTime();
+            long min=(date.getTime()-date1.getTime())/60000;
+            if(min<=15){
+                tarlist.add(collectInfoValue);
+            }
+        }
+        return tarlist;
+    }
+    @Override
+    public  List<CollectInfoValue> StatisticalOneHour(java.util.Date  date) {
+        List<CollectInfoValue> list=collectInfoValueMapper.selectByExample(null);
+        List<CollectInfoValue> tarlist=new ArrayList<CollectInfoValue>();
+        Iterator<CollectInfoValue> it=list.iterator();
+        while(it.hasNext()){
+            CollectInfoValue collectInfoValue=it.next();
+            java.util.Date date1=collectInfoValue.getCollectTime();
+            long min=(date.getTime()-date1.getTime())/60000;
+            if(min<=60){
+                tarlist.add(collectInfoValue);
+            }
+        }
+        return tarlist;
+    }
+
+    @Override
+    public  List<CollectInfoValue> StatisticalOneDay(java.util.Date  date) {
+        List<CollectInfoValue> list=collectInfoValueMapper.selectByExample(null);
+        List<CollectInfoValue> tarlist=new ArrayList<CollectInfoValue>();
+        Iterator<CollectInfoValue> it=list.iterator();
+        while(it.hasNext()){
+            CollectInfoValue collectInfoValue=it.next();
+            java.util.Date date1=collectInfoValue.getCollectTime();
+            long hour=(date.getTime()-date1.getTime())/60000/60;
+            if(hour<=24){
+                tarlist.add(collectInfoValue);
+            }
+        }
+        return tarlist;
+    }
+
+    @Override
+    public  List<CollectInfoValue>  StatisticalOneWeek(java.util.Date  date) {
+        List<CollectInfoValue> list=collectInfoValueMapper.selectByExample(null);
+        List<CollectInfoValue> tarlist=new ArrayList<CollectInfoValue>();
+        Iterator<CollectInfoValue> it=list.iterator();
+        while(it.hasNext()){
+            CollectInfoValue collectInfoValue=it.next();
+            java.util.Date date1=collectInfoValue.getCollectTime();
+            long day=(date.getTime()-date1.getTime())/60000/60/24;
+            if(day<=7){
+                tarlist.add(collectInfoValue);
+            }
+        }
+        return tarlist;
+    }
+}
