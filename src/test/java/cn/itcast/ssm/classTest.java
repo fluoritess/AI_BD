5700422110083fb79d8a4e11e4afbfe17b5dba16
肖威 <1378407417@qq.com>
2018-12-29 20:30:54 +0800

    asdfsadfdsafds

--- src/test/java/cn/itcast/ssm/classTest.java
+++ src/test/java/cn/itcast/ssm/classTest.java
@@ -2,11 +2,9 @@ package cn.itcast.ssm;
 
 import cn.itcast.ssm.service.impl.WarningDeviceImpl;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.xml.ws.Action;
+
 
 /**
  * @author tyh
