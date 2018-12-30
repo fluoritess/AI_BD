182b7954c2816da4d648fa847c8cbda4cc4c8c06
cdu_liu_yang <1138556554@qq.com>
2018-12-30 19:01:32 +0800

    Merge branch 'development' of https://gitee.com/Dust511/AI_BD
    # Conflicts:
    #	src/main/java/cn/itcast/ssm/controller/StatisticalAnalysisController.java
    #	src/main/java/cn/itcast/ssm/service/StatisticalAnalysisService.java
    #	src/main/java/cn/itcast/ssm/service/impl/StatisticalAnalysisServiceImpl.java

--- src/main/java/cn/itcast/ssm/netty/DiscardServerHandler.java
+++ src/main/java/cn/itcast/ssm/netty/DiscardServerHandler.java
@@ -1,9 +1,11 @@
 package cn.itcast.ssm.netty;
 
+import cn.itcast.ssm.po.CollectInfoValue;
 import cn.itcast.ssm.service.BaseService;
 import cn.itcast.ssm.service.EquipmentService;
 import cn.itcast.ssm.service.impl.BaseServiceImpl;
 import cn.itcast.ssm.service.impl.EquipmentServiceImpl;
+import cn.itcast.ssm.util.Constant;
 import cn.itcast.ssm.util.test;
 import com.corundumstudio.socketio.SocketIOClient;
 import io.netty.buffer.ByteBuf;
@@ -22,6 +24,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.Buffer;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +37,7 @@ import java.util.Map;
 
 
 @Component
-public class DiscardServerHandler extends  ChannelInboundHandlerAdapter{
+public class DiscardServerHandler extends  ChannelInboundHandlerAdapter implements Constant {
 
     @Autowired
     BaseService baseService;
@@ -48,7 +51,7 @@ public class DiscardServerHandler extends  ChannelInboundHandlerAdapter{
     public void channelRead(ChannelHandlerContext ctx, Object msg)  {
         try {
 
-
+Map<String,Object>  sennorValueMap=new HashMap<>();
 
             List msg1=(List)msg;
             System.out.println("传输内容是");
@@ -58,8 +61,37 @@ public class DiscardServerHandler extends  ChannelInboundHandlerAdapter{
 
             for (Map.Entry<String, SocketIOClient> entry : EventListennter.clients1.entrySet()){
                 if(entry.getKey().equals(msg1.get(3))){
+                    for(Map.Entry<String, Object> entry1 : map.entrySet()){
+                        if ("土壤根系温度".equals(entry1.getKey())){
+                        sennorValueMap.put(entry1.getValue().toString(),msg1.get(4));
+                        }
+                        else  if ("土壤表层温度".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(5));
+                        } else if ("土壤根系湿度".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(6));
+                        }
+                        else if ("土壤表层湿度".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(7));
+                        }
+                        else if ("温室温度".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(8));
+                        }
+                        else if ("温室湿度".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(9));
+                        }
+                        else if ("温室气压".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(10));
+                        }
+                        else if ("温室二氧化碳浓度".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(11));
+                        }
+                        else if ("温室通风风速".equals(entry1.getKey())){
+                            sennorValueMap.put(entry1.getValue().toString(),msg1.get(12));
+                        }
+
+                    }
                     System.out.println("sendEvent");
-                    entry.getValue().sendEvent("onGetValue",msg1.get(1));
+                    entry.getValue().sendEvent("onGetValue",sennorValueMap);
                 }
             }
 
--- src/main/java/cn/itcast/ssm/service/impl/BaseServiceImpl.java
+++ src/main/java/cn/itcast/ssm/service/impl/BaseServiceImpl.java
@@ -3,9 +3,11 @@ package cn.itcast.ssm.service.impl;
 import cn.itcast.ssm.mapper.*;
 import cn.itcast.ssm.po.*;
 import cn.itcast.ssm.service.BaseService;
+import cn.itcast.ssm.util.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 import java.util.List;
 
@@ -16,7 +18,7 @@ import java.util.List;
  * Modified By:
  */
 @Service
-public class BaseServiceImpl implements BaseService {
+public class BaseServiceImpl implements BaseService,Constant {
 
     @Autowired
     CollectInfoValueMapper collectInfoValueMapper;
@@ -24,6 +26,7 @@ public class BaseServiceImpl implements BaseService {
     NodedeviceSensorconfigInfoMapper nodedeviceSensorconfigInfoMapper;
     @Override
     public void test(List add) {
+
         NodedeviceSensorconfigInfoExample  nodedeviceSensorconfigInfoExample = new NodedeviceSensorconfigInfoExample();
         NodedeviceSensorconfigInfoExample.Criteria criteria=nodedeviceSensorconfigInfoExample.createCriteria();
         criteria.andSensorPositionEqualTo("土壤根系温度");
@@ -31,7 +34,9 @@ public class BaseServiceImpl implements BaseService {
         Date day=new Date();
         Date date = new Date(day.getTime());
         for (NodedeviceSensorconfigInfo nodedeviceSensorconfigInfo:nodedeviceSensorconfigInfos){
+
            if ("土壤根系温度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("土壤根系温度",nodedeviceSensorconfigInfo.getSensorId());
                CollectInfoValue collectInfoValue = new CollectInfoValue();
                collectInfoValue.setCollectTime(date);
                collectInfoValue.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
@@ -39,18 +44,68 @@ public class BaseServiceImpl implements BaseService {
                collectInfoValueMapper.insertSelective(collectInfoValue);
             }
             else  if ("土壤表层温度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("土壤表层温度",nodedeviceSensorconfigInfo.getSensorId());
                 CollectInfoValue collectInfoValue1 = new CollectInfoValue();
                 collectInfoValue1.setCollectTime(date);
                 collectInfoValue1.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
                 collectInfoValue1.setSensorValue(Float.parseFloat((String)add.get(5)));
                 collectInfoValueMapper.insertSelective(collectInfoValue1);
             } else if ("土壤根系湿度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
-                CollectInfoValue collectInfoValue2 = new CollectInfoValue();
-                collectInfoValue2.setCollectTime(date);
-                collectInfoValue2.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
-                collectInfoValue2.setSensorValue(Float.parseFloat((String)add.get(6)));
-                collectInfoValueMapper.insertSelective(collectInfoValue2);
+               map.put("土壤根系湿度",nodedeviceSensorconfigInfo.getSensorId());
+                CollectInfoValue collectInfoValue3 = new CollectInfoValue();
+                collectInfoValue3.setCollectTime(date);
+                collectInfoValue3.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+                collectInfoValue3.setSensorValue(Float.parseFloat((String)add.get(6)));
+                collectInfoValueMapper.insertSelective(collectInfoValue3);
             }
+           else if ("土壤表层湿度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("土壤表层湿度",nodedeviceSensorconfigInfo.getSensorId());
+               CollectInfoValue collectInfoValue4 = new CollectInfoValue();
+               collectInfoValue4.setCollectTime(date);
+               collectInfoValue4.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+               collectInfoValue4.setSensorValue(Float.parseFloat((String)add.get(7)));
+               collectInfoValueMapper.insertSelective(collectInfoValue4);
+           }
+           else if ("温室温度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("温室温度",nodedeviceSensorconfigInfo.getSensorId());
+               CollectInfoValue collectInfoValue5 = new CollectInfoValue();
+               collectInfoValue5.setCollectTime(date);
+               collectInfoValue5.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+               collectInfoValue5.setSensorValue(Float.parseFloat((String)add.get(8)));
+               collectInfoValueMapper.insertSelective(collectInfoValue5);
+           }
+           else if ("温室湿度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("温室湿度",nodedeviceSensorconfigInfo.getSensorId());
+               CollectInfoValue collectInfoValue6 = new CollectInfoValue();
+               collectInfoValue6.setCollectTime(date);
+               collectInfoValue6.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+               collectInfoValue6.setSensorValue(Float.parseFloat((String)add.get(9)));
+               collectInfoValueMapper.insertSelective(collectInfoValue6);
+           }
+           else if ("温室气压".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("温室气压",nodedeviceSensorconfigInfo.getSensorId());
+               CollectInfoValue collectInfoValue7 = new CollectInfoValue();
+               collectInfoValue7.setCollectTime(date);
+               collectInfoValue7.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+               collectInfoValue7.setSensorValue(Float.parseFloat((String)add.get(10)));
+               collectInfoValueMapper.insertSelective(collectInfoValue7);
+           }
+           else if ("温室二氧化碳浓度".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("温室二氧化碳浓度",nodedeviceSensorconfigInfo.getSensorId());
+               CollectInfoValue collectInfoValue8 = new CollectInfoValue();
+               collectInfoValue8.setCollectTime(date);
+               collectInfoValue8.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+               collectInfoValue8.setSensorValue(Float.parseFloat((String)add.get(11)));
+               collectInfoValueMapper.insertSelective(collectInfoValue8);
+           }
+           else if ("温室通风风速".equals(nodedeviceSensorconfigInfo.getSensorPosition())){
+               map.put("温室通风风速",nodedeviceSensorconfigInfo.getSensorId());
+               CollectInfoValue collectInfoValue9 = new CollectInfoValue();
+               collectInfoValue9.setCollectTime(date);
+               collectInfoValue9.setSensorId(nodedeviceSensorconfigInfo.getSensorId());
+               collectInfoValue9.setSensorValue(Float.parseFloat((String)add.get(12)));
+               collectInfoValueMapper.insertSelective(collectInfoValue9);
+           }
 
         }
 
